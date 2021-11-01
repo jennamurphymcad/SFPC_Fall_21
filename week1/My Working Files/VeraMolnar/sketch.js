53ef2f988a23404a89796e3accfb06d1bdf3ed83
Jenna Murphy <jennadeemurphy@gmail.com>
2021-10-31 21:28:21 -0500

    sketch files added - was originally working in p5.js editor but transfered into html and .js files)

---
+++ week1/My Working Files/VeraMolnar/sketch.js
@@ -0,0 +1,30 @@
+function setup() {
+    createCanvas(1000, 1000);
+    noLoop();
+  }
+  
+  function draw() {
+     background(255);
+    colorMode(RGB, 255, 255, 255, 1);
+    translate(width-900, 0);
+    strokeWeight(.35);
+  
+    
+    for (var x=0; x <= width; x += width/75) {
+      if (x <= 500) {
+        fill(0,0,0,.7);
+        // stroke(255);
+      } else {
+        noFill();
+        // stroke(0);
+        // strokeWeight(.8);
+      }
+      rect(x*.45,200+random(-30,40),5,200);
+      rect(x*.45,400+random(-40,40),5,200);
+      rect(x*.45,600+random(-30,40),5,200);
+    }
+  }
+
+function keyReleased() {
+  if (key == 's' || key == 'S') saveCanvas(gd.timestamp(), 'png');
+}
